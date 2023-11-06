e0bc0e259d00d0a456697a8826e4563728219ce7
hongquan15 <nhquan1509@gmail.com>
2023-11-06 10:25:27 +0700

    create splash

---
+++ app/src/main/java/com/example/projectprm392/activity/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.projectprm392.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectprm392.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
